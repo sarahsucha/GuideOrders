c3696f8189968e985fa41ccb780e2c3bf057ee4e
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-08-10 14:52:39 -0700

    Add note about refactoring routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,11 +6,15 @@ get '/users/:user_id/orders/new' do
 end
 
 # Orders CREATE
+# post '/users/:id/orders'
 post '/orders' do
   #print out the date to see if in a format which can save to db
   #will get a number like $14.00 need to convert to 1400 to save to db
   p "I'm posting in orders"
   p (params)
+  # create a new customer
+  # create a new order
+  # create new order items
   redirect "/users/#{session.id}/orders"
 end
 
