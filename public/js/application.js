cf8f19ec931378074ad1e2ce0573bc45e3a811f1
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-08-15 22:36:28 -0700

    Update CSS for menu and order summaries

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-
-  $('.nav li').on('click', function() {
-    $('.nav li').removeClass('active');
-    $(this).addClass('active');
-  })
+  // $('.nav li').on('click', function() {
+  //   $('.nav li').removeClass('active');
+  //   $(this).addClass('active');
+  // })
 
   var path = window.location.pathname;
   console.log(path)
@@ -17,9 +12,7 @@ $(document).ready(function() {
   //   path = '/';
   // }
 
-  $('.nav [href="' + path + '"]').on('load', function() {
-    $(this).addClass('active');
-  })
+  $('.nav [href="' + path + '"]').parent().addClass('active');
 
   // $('.nav [href="' + path + '"]').addClass('active');
   // target.addClass('active');
